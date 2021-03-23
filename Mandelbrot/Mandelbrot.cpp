061fad607c2919210b4c9259d4ac49986318a4e6
Pierre Mollard <58636597+Pierre-Mollard@users.noreply.github.com>
2021-03-23 11:33:41 +0100

    Added Prof Time Display Cleaned Main()

--- Mandelbrot/Mandelbrot.cpp
+++ Mandelbrot/Mandelbrot.cpp
@@ -71,7 +71,6 @@ WCHAR szWindowClass[MAX_LOADSTRING];            // nom de la classe de fenêtre 
 HWND hTextInput;           // nom de la classe de fenêtre principale
 HWND hTextOutput;
 HDC hdc;
-void* memory;
 unsigned int* grid;
 int imgWIDTH = 1000;
 int imgHEIGHT = 1000;
@@ -89,6 +88,8 @@ cl_command_queue commands;
 cl_program program;
 cl_context context;
 cl_mem y_out;
+cl_ulong ev_start_time = (cl_ulong)0;
+cl_ulong ev_end_time = (cl_ulong)0;
 
 // Déclarations anticipées des fonctions incluses dans ce module de code :
 ATOM                MyRegisterClass(HINSTANCE hInstance);
@@ -195,6 +196,26 @@ void saveBMP(const char* name, int width, int height, unsigned int* data, int ma
     fclose(f);
 }
 
+int sendKernel() {
+    cl_int err;
+
+    err = 0;
+    err = clSetKernelArg(kernel, 0, sizeof(cl_double), &startX);
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_double), &startY);
+    err |= clSetKernelArg(kernel, 2, sizeof(cl_double), &step);
+    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &maxIter);
+    err |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &y_out);
+    err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &imgWIDTH);
+
+    err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, dim, NULL, 0, NULL, &prof_event);
+    
+    clFinish(commands);
+
+    err = clEnqueueReadBuffer(commands, y_out, CL_TRUE, 0, sizeof(unsigned int) * imgHEIGHT * imgWIDTH, grid, 0, NULL, NULL);
+
+    return err;
+}
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
@@ -210,24 +231,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     bitmap_info.bmiHeader.biBitCount = 32;
     bitmap_info.bmiHeader.biCompression = BI_RGB;
 
-    
-    // TODO: Placez le code ici.
-
-    size_t global;                      // global domain size  
-    size_t local;                       // local  domain size  
-
-    int nworkgroup = 32;
-    int max_size = 16;
-    int workgroup_size = max_size;
-    int i;
-
-    int ntotal_iter = imgHEIGHT * imgWIDTH;
-
     float xmax = 1.5;
     float xmin = -2;
     float ymax = 1.75f;
     float ymin = -1.75f;
-
 
     cl_context_properties properties[3];
     cl_int err;
@@ -237,7 +244,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     cl_uint num_of_devices = 0;
 
     grid = (unsigned int*)malloc(imgWIDTH * imgHEIGHT * sizeof(unsigned int));
-    //memory = VirtualAlloc(grid, imgWIDTH * imgHEIGHT * sizeof(unsigned int), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 
     clGetPlatformIDs(1, &platform_id, &num_of_platform);
     clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &num_of_devices);
@@ -246,94 +252,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     properties[1] = (cl_context_properties)platform_id;
     properties[2] = 0;
 
-
     context = clCreateContext(properties, 1, &device_id, NULL, NULL, &err);
     commands = clCreateCommandQueue(context, device_id, CL_QUEUE_PROFILING_ENABLE, &err);
     program = clCreateProgramWithSource(context, 1, (const char**)&KernelSource, NULL, &err);
     clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     kernel = clCreateKernel(program, "mandel", &err);
 
-    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(max_size), &max_size, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to retrieve kernel work group info! %d\n", err);
-    }
-    if (max_size > workgroup_size) workgroup_size = max_size;
-
-    // Now that we know the size of the work_groups, we can set the number of work
-    // groups, the actual number of steps, and the step size
-    nworkgroup = ntotal_iter / (workgroup_size);
-
-    if (nworkgroup < 1)
-    {
-        int comp_units;
-        err = clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(cl_uint), &comp_units, NULL);
-        if (err != CL_SUCCESS)
-        {
-            printf("Error: Failed to access device number of compute units !\n");
-            return EXIT_FAILURE;
-        }
-        nworkgroup = comp_units;
-        workgroup_size = ntotal_iter / (nworkgroup);
-    }
-    int nsteps = workgroup_size * nworkgroup;
-    step = 1.0 / (double)nsteps;
     step = 0.0025;
-
-    printf("Total iter : %d || Nstep : %d\n", ntotal_iter, nsteps);
-    printf(" %d work groups of size %d.  %d Integration steps\n", (int)nworkgroup, (int)workgroup_size, nsteps);
 
     y_out = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(unsigned int) * imgHEIGHT * imgWIDTH, NULL, NULL);
 
-    // Set the arguments to our compute kernel
-    err = 0;
-    err = clSetKernelArg(kernel, 0, sizeof(cl_double), &startX);
-    err |= clSetKernelArg(kernel, 1, sizeof(cl_double), &startY);
-    err |= clSetKernelArg(kernel, 2, sizeof(cl_double), &step);
-    err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &maxIter);
-    err |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &y_out);
-    err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &imgWIDTH);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to set kernel arguments! %d\n", err);
-        exit(1);
-    }
-
-    double rtime;
-    rtime = clock();
-
-    err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, dim, NULL, 0, NULL, &prof_event);
-    if (err)
-    {
-        printf("Error: Failed to execute kernel!\n");
-        return EXIT_FAILURE;
-    }
-
-    // Wait for the commands to complete before reading back results
-    clFinish(commands);
-
-
-    // Read back the results from the compute device
-    err = clEnqueueReadBuffer(commands, y_out, CL_TRUE, 0, sizeof(unsigned int) * imgHEIGHT * imgWIDTH, grid, 0, NULL, NULL);
-    if (err != CL_SUCCESS)
-    {
-        printf("Error: Failed to read output array! %d\n", err);
-        exit(1);
-    }
-
-    // extract timing data from the event, prof_event
-    err = clWaitForEvents(1, &prof_event);
-    cl_ulong ev_start_time = (cl_ulong)0;
-    cl_ulong ev_end_time = (cl_ulong)0;
-    err = clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START,
-        sizeof(cl_ulong), &ev_start_time, NULL);
-    err = clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END,
-        sizeof(cl_ulong), &ev_end_time, NULL);
-
-    wchar_t * tempL[32];
-    //swprintf_s(&tempL[32], L"prof says %f ms \n", (double)(ev_end_time - ev_start_time) * 1.0e-6);
-    
-
+    sendKernel();
 
     // Initialise les chaînes globales
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -343,14 +272,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // Effectue l'initialisation de l'application :
     if (!InitInstance (hInstance, nCmdShow))
     {
-
         return FALSE;
     }
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MANDELBROT));
 
     MSG msg;
-   
 
     // Boucle de messages principale :
     while (GetMessage(&msg, nullptr, 0, 0))
@@ -364,8 +291,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     return (int) msg.wParam;
 }
-
-void saveBMP();
 
 //
 //  FONCTION : MyRegisterClass()
@@ -462,37 +387,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 DestroyWindow(hWnd);
                 break;
             case BT_NDRANGE:
-                cl_int err;
+                
+                sendKernel();
 
-                err = 0;
-                err = clSetKernelArg(kernel, 0, sizeof(cl_double), &startX);
-                err |= clSetKernelArg(kernel, 1, sizeof(cl_double), &startY);
-                err |= clSetKernelArg(kernel, 2, sizeof(cl_double), &step);
-                err |= clSetKernelArg(kernel, 3, sizeof(unsigned int), &maxIter);
-                err |= clSetKernelArg(kernel, 4, sizeof(cl_mem), &y_out);
-                err |= clSetKernelArg(kernel, 5, sizeof(unsigned int), &imgWIDTH);
-                if (err != CL_SUCCESS)
-                {
-                    MessageBox(hWnd, L"ERROR", L"Error: Failed to set kernel arguments!", MB_OK);
-                }
+                // extract timing data from the event, prof_event
+                clWaitForEvents(1, &prof_event);
+                clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &ev_start_time, NULL);
+                clGetEventProfilingInfo(prof_event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &ev_end_time, NULL);
 
-                step -= 0.0001;
-                startY += 0.01;
-                err = clEnqueueNDRangeKernel(commands, kernel, 2, NULL, dim, NULL, 0, NULL, &prof_event);
-                if (err != CL_SUCCESS)
-                {
-                    
-                }
-                clFinish(commands);
-                err = clEnqueueReadBuffer(commands, y_out, CL_TRUE, 0, sizeof(unsigned int) * imgHEIGHT * imgWIDTH, grid, 0, NULL, NULL);
-                if (err != CL_SUCCESS)
-                {
-                    MessageBox(hWnd, L"ERROR", L"Error: Failed to read output array!", MB_OK);
-                }
-
-                wchar_t text[100];
-                GetWindowTextW(hTextInput, text, 100);
-                SetWindowTextW(hTextOutput, L"Done.  (TODO Time)");
+                wchar_t buff[32];
+                swprintf_s(buff, 32, L"Prof Time : %fms \n", (double)(ev_end_time - ev_start_time) * 1.0e-6);
+                SetWindowTextW(hTextOutput, buff);
 
                 //Repaint Window
                 InvalidateRect(hWnd, NULL, TRUE);
@@ -515,50 +420,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             hdc = BeginPaint(hWnd, &ps);
-            // TODO: Ajoutez ici le code de dessin qui utilise hdc...
-
-            /*
-            int i, j, iter = 0;
-            if (grid != NULL) {
-                for (j = 0; j < imgHEIGHT; j++) {
-                    for (i = 0; i < imgWIDTH; i++) {
-                        iter++;
-                        int mod = (unsigned char)*(grid + iter);
-                        int r, g, b = 0;
-                        switch (mod)
-                        {
-                                case 0: r = 66; g = 30; b = 15; break;
-                                case 1: r = 25; g = 7; b = 26; break;
-                                case 2: r = 9; g = 1; b = 47; break;
-                                case 3: r = 4; g = 4; b = 73; break;
-                                case 4: r = 0; g = 7; b = 100; break;
-                                case 5: r = 12; g = 44; b = 138; break;
-                                case 6: r = 24; g = 82; b = 177; break;
-                                case 7: r = 57; g = 125; b = 209; break;
-                                case 8: r = 134; g = 181; b = 229; break;
-                                case 9: r = 211; g = 236; b = 248; break;
-                                case 10: r = 241; g = 233; b = 191; break;
-                                case 11: r = 248; g = 201; b = 95; break;
-                                case 12: r = 254; g = 170; b = 0; break;
-                                case 13: r = 204; g = 128; b = 0; break;
-                                case 14: r = 153; g = 87; b = 0; break;
-                                case 15: r = 106; g = 52; b = 3; break;
-                        }
-                        SetPixel(hdc, gridOffsetX + i, gridOffsetY + j, RGB(r, g, b));
-
-                        
-                    }
-                }
-            }
-            */
+            
             StretchDIBits(hdc, gridOffsetX, gridOffsetY, imgWIDTH, imgHEIGHT, 0, 0, imgWIDTH, imgHEIGHT, grid, &bitmap_info, DIB_RGB_COLORS, SRCCOPY);
-
 
             EndPaint(hWnd, &ps);
         }
         break;
     case WM_DESTROY:
-        // cleanup then shutdown
         clReleaseMemObject(y_out);
         clReleaseProgram(program);
         clReleaseKernel(kernel);
